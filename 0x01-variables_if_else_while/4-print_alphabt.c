edde13b945ac4cfeb81a1fe7d0d7129e4485edc2
adesoji-ii <festusoluwapelumi@gmail.com>
2023-08-11 16:46:22 -0700

    Alphabets in lower case

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print alphabets in lowercase
+ * Return: o (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <== 'z') {
+			if ((c != 'q' && c != 'e') && c <='z')
+				putchar(c);
+		}
+	putchar('\n');
+	return(0)
+
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digit numbers of base10
+ * Return 0 (Success)
+ */
+int main(void)
