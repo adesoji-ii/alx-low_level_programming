5ac33958a08ce8d2b15a7d8dd77b5c9c05713f6d
adesoji-ii <festusoluwapelumi@gmail.com>
2023-08-11 15:42:35 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print alphabet in lower case
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= '2') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
