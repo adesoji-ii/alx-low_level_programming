1e2cb544c4c5308d58280aed1faab1309caa12ca
adesoji-ii <festusoluwapelumi@gmail.com>
2023-08-11 17:57:49 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Print single digits numners of base10
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(C >= 'a'); {
+			putchar(c);
+			c--;
+		}
+	putchar("\n");
+	return (0);
+	}
