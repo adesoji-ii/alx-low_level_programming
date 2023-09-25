9ab2d6d1c85aee9d1bc5d9f25e868d75b13a1c5a
adesoji-ii <festusoluwapelumi@gmail.com>
2023-09-25 11:36:03 -0700

    fix

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * listint_len - a function that returns the number of elements in a linked list
+ * listint_len - a function that returns the number of elements
+ * in a linked list
  * @h: linked list of type listint_len to traverse
  * Return: total number of nodes counted
  */
