129fe974ca5363caa8028c94b0787e0e4e66d96e
adesoji-ii <festusoluwapelumi@gmail.com>
2023-12-13 13:50:51 -0800

    dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/adesoji-ii/ alx-low_level_programming/0x18-dynamic_libraries/lib.so
+wget -P /tmp/ https://raw.github.com/adesoji-ii/alx-low_level_programming/0x18-dynamic_libraries/lib.so
 export LD_PRELOAD=/tmp/lib.so
